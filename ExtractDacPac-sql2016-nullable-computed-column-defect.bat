343f7412656a7698119f23f8a5514c39cfd16459
AndyFenna <andrew.fenna@laterooms.com>
2018-10-17 13:22:07 +0100

    Initial commit

---
+++ ExtractDacPac-sql2016-nullable-computed-column-defect.bat
@@ -0,0 +1,2 @@
+"C:\Program Files (x86)\Microsoft Visual Studio\2017\Professional\Common7\IDE\Extensions\Microsoft\SQLDB\DAC\150\sqlpackage.exe" /Action:Extract /OverwriteFiles:True /TargetFile:".\ExtractedDacPac\sql2016-nullable-computed-column-defect.dacpac" /SourceDatabaseName:sql2016-nullable-computed-column-defect /SourceServerName:.
+PAUSE
---
+++ sql2016-nullable-computed-column-defect/ATable.sql
@@ -0,0 +1,6 @@
+﻿CREATE TABLE [dbo].[ATable]
+(
+	[ID] INT NOT NULL PRIMARY KEY,
+	[ADate] DATE NOT NULL,
+    [DayOfYear] AS (dbo.[CalcDayOfYear]([ADate])) PERSISTED
+)
